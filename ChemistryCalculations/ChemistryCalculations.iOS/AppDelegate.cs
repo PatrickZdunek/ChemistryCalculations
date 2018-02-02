b159a0f23e6c934889e85e446228f7442e97f513
Patrick Zdunek <zdunek@simulationdynamics.com>
2018-02-02 14:07:04 -0500

    First Commit

---
+++ ChemistryCalculations/ChemistryCalculations.iOS/AppDelegate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using UIKit;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace ChemistryCalculations.iOS
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the 
+    // User Interface of the application, as well as listening (and optionally responding) to 
+    // application events from iOS.
+    [Register("AppDelegate")]
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        //
+        // This method is invoked when the application has loaded and is ready to run. In this 
+        // method you should instantiate the window, load the UI into it and then make the window
+        // visible.
+        //
+        // You have 17 seconds to return from this method, or iOS will terminate your application.
+        //
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            global::Xamarin.Forms.Forms.Init();
+            LoadApplication(new App());
+            AppCenter.Start("9bf10c45-574e-4a0b-91b8-d0f71a5922c2",
+                   typeof(Analytics), typeof(Crashes));
+            return base.FinishedLaunching(app, options);
+        }
+    }
+}
